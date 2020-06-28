27b376855552cec4b5eac60bfae0aa95497e6bcd
lpanonymous <lpanonymous0101@gmail.com>
2020-06-27 20:24:55 -0500

    cambios

--- index.php
+++ index.php
@@ -20,15 +20,12 @@
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
-      <a class="navbar-brand" href="#">Aerolinea soap</a>
+      <a class="navbar-brand" href="#">Aerolinea</a>
 
       <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
         <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
           <li class="nav-item active">
-            <a class="nav-link" href="views/hotel.php">Hotel soap<span class="sr-only">(current)</span></a>
-          </li>
-          <li class="nav-item active">
-            <a class="nav-link" href="indexrest.php">Rest<span class="sr-only">(current)</span></a>
+            <a class="nav-link" href="views/hotel.php">Hotel<span class="sr-only">(current)</span></a>
           </li>
         </ul>
       </div>

d257705cce963f45688a4d39817950bf1baeffcc
lpanonymous <lpanonymous0101@gmail.com>
2020-07-08 00:36:20 -0500

    cambios en las vistas

--- controllers/flydetail.php
+++ controllers/flydetail.php
@@ -17,12 +17,12 @@
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
-      <a class="navbar-brand" href="#">Detalles del vuelo</a>
+      <a class="navbar-brand" href="../index.php">Aerolinea</a>
 
       <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
         <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
           <li class="nav-item active">
-            <a class="nav-link" href="../index.php">Aerolinea <span class="sr-only">(current)</span></a>
+            <a class="nav-link" href="#">Detalles del vuelo <span class="sr-only">(current)</span></a>
           </li>
         </ul>
       </div>
--- views/reserveroom.php
+++ views/reserveroom.php
@@ -38,35 +38,37 @@
 </head>
 <body>
 <nav class="navbar navbar-expand-lg navbar navbar-dark bg-dark">
+      <a class="navbar-brand" href="hotel.php">Hotel</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
-      <a class="navbar-brand" href="#">Reservar habitación</a>
-
       <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
         <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-        <li class="nav-item active">
+          <li class="nav-item">
             <a class="nav-link" href="registerhotel.php">Registrar cliente <span class="sr-only">(current)</span></a>
           </li>
           <li class="nav-item active">
+            <a class="nav-link" href="reserveroom.php">Reservar habitación <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
             <a class="nav-link" href="payreservation.php">Pagar reservación <span class="sr-only">(current)</span></a>
           </li>
-          <li class="nav-item active">
+          <li class="nav-item">
             <a class="nav-link" href="roomavailability.php">Disponibilidad de habitación<span class="sr-only">(current)</span></a>
           </li>
-          <li class="nav-item active">
+          <li class="nav-item">
             <a class="nav-link" href="editclient.php">Editar cliente<span class="sr-only">(current)</span></a>
           </li>
-          <li class="nav-item active">
+          <li class="nav-item">
             <a class="nav-link" href="showcustomerinfo.php">Información de cliente <span class="sr-only">(current)</span></a>
           </li>
-          <li class="nav-item active">
+          <li class="nav-item">
             <a class="nav-link" href="deleteclient.php">Eliminar cliente<span class="sr-only">(current)</span></a>
           </li>
-          <li class="nav-item active">
+          <li class="nav-item">
             <a class="nav-link" href="deletereservation.php">Eliminar reservación <span class="sr-only">(current)</span></a>
           </li>
-          <li class="nav-item active">
+          <li class="nav-item">
             <a class="nav-link" href="../index.php">Aerolinea <span class="sr-only">(current)</span></a>
           </li>
         </ul>
